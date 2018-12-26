c554a8f55ea92ea67e837648558ffbdee9154256
eric.mo <mozs@xiaopeng.com>
2018-12-26 22:48:31 +0800

    springmvc

---
+++ src/main/java/org/springstudy/webapp/controller/HelloController.java
@@ -0,0 +1,25 @@
+package org.springstudy.webapp.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Eric.Mo
+ * @since 2018/12/26
+ */
+@Slf4j
+@Controller
+@RestController
+public class HelloController {
+
+    @RequestMapping(value = "/health_check", method = RequestMethod.GET)
+    public ResponseEntity<String> healthCheck() {
+        log.info("health_check: ok");
+        return new ResponseEntity<String>("ok", HttpStatus.OK);
+    }
+}
