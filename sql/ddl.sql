347ee045438321fb3ea950e5a7a512985a621bc7
sheng <493403198@qq.com>
2019-02-19 11:08:15 +0800

    1.3.8 -- 添加每日详细表

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -46,3 +46,21 @@ CREATE TABLE `month_bill` (
     `end_date` varchar(4) NOT NULL DEFAULT '' COMMENT '账单周期结束日期',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB CHARSET = utf8 COMMENT '每月余额表';
+
+
+DROP TABLE IF EXISTS `day_bill`;
+CREATE TABLE `month_bill` (
+    `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '自增id',
+    `user_id` varchar(64) NOT NULL DEFAULT '' COMMENT '用户 id',
+    `dr_amount` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '借方金额',
+    `cr_amount` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '贷方金额',
+    `balance` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '余额,如钱包余额',
+    `account_id` bigint(20) UNSIGNED NOT NULL DEFAULT 0 COMMENT '账户id',
+    `transaction_type` varchar(10) NOT NULL DEFAULT '0' COMMENT '交易类型',
+    `remark` varchar(64) NOT NULL DEFAULT '' COMMENT '备注',
+    `tx_month` varchar(6) NOT NULL DEFAULT '' COMMENT '交易月,如201901',
+    `tx_day` varchar(6) NOT NULL DEFAULT '' COMMENT '交易日,如0125',
+    `start_date` varchar(4) NOT NULL DEFAULT '' COMMENT '账单周期开始日期',
+    `end_date` varchar(4) NOT NULL DEFAULT '' COMMENT '账单周期结束日期',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB CHARSET = utf8 COMMENT '每日余额表';
