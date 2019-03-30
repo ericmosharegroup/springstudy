dc4a645fcb5ae51cf21a9829e92d5bc06c417a8d
sheng <493403198@qq.com>
2019-03-30 22:06:00 +0800

    1.4.8 -- 添加月账单周期

--- src/main/webapp/frontend/static/script/sys.js
+++ src/main/webapp/frontend/static/script/sys.js
@@ -60,3 +60,19 @@ function isEmpty(str) {
         return true;
     }
 }
+
+
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    return year + seperator1 + month + seperator1 + strDate;
+}
